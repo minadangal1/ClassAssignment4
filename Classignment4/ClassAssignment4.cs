b020f5cfccce67ebae21418c3134511a071cce7c
minadangal1 <amritsa2012@gmail.com>
2021-01-01 16:21:15 -0600

    initial commit

---
+++ Classignment4/ClassAssignment4.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classignment4
+{
+    class ClassAssignment4
+    {
+        public string Hello()
+
+
+        {
+            String s = "hello";
+
+            string input1 = Console.ReadLine();
+          
+            if (input1.StartsWith("hello"))
+            {
+
+
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+           // Console.WriteLine("input string:" +input1);
+
+      
+
+
+           
+           // Console.WriteLine(input1.StartsWith("hello"));
+
+            return input1;
+
+
+
+
+        
+        }
+         public static void cubeNumber()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int cube;
+            
+            for (int i = 0; i <= n; i++)
+            {
+
+                cube = i * i * i;
+
+                Console.Write("\nNumber is : "+i+" and cube of the :" +i +".is."+cube);
+            }
+        }
+        public static void pattern()
+        {
+            Console.WriteLine("Display the pattern like right angle triangle");
+            Console.Write("input the number of raw:");
+            int row = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i = 1; i <= row; i++)
+            {
+                for(int j=1;j <= i; j++)
+                
+                    Console.Write("{0}",j);
+                    Console.Write("\n");
+                
+               // Console.WriteLine("\n");
+            }
+            //Console.WriteLine("\n");
+
+        }
+        public static void swarp()
+        {
+            Console.WriteLine("value before swap");
+            int number1 = 2;
+            Console.WriteLine("Enter the First Number : "+number1);
+            int number2 = 3;
+            
+            Console.WriteLine("Enter the Second Number : " +number2);
+            
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            Console.WriteLine("after swap");
+            
+            Console.WriteLine("\nfirst number is:  " + number1 + " \n second number is:" + number2);
+
+            
+        }
+
+    }
+}
---
+++ Classignment4/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Classignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remove();
+            //subString();
+            // longestWord();
+           // sumOfPrimeNumber();
+            // sumOfDegiete();
+            // Array();
+            ClassAssignment4 c = new ClassAssignment4();
+             c.Hello();
+            // ClassAssignment4.cubeNumber();
+            // ClassAssignment4.pattern();
+             ClassAssignment4.swarp();
+        }
+
+        public static void Remove()
+        {
+            Console.WriteLine("orginal value before remove");
+            string input = Console.ReadLine();
+            string value = input.Replace("1111", "");
+
+            Console.WriteLine(" new value after replace:" + value);
+        }
+        static void subString()
+        {
+            int startindex = 0;
+            int length = 1;
+
+            Console.Write("Input a string : ");
+            string input = Console.ReadLine();
+
+
+            var s = input.Substring(startindex, length);
+            Console.WriteLine(s + input + s);
+
+        }
+        static void longestWord()
+        {
+            string data = "write a c# program to display the fllowing pattern using the alphabet";
+            int startindex = 34;
+            int length = 8;
+
+
+            string newData = data.Substring(startindex, length);
+
+            Console.WriteLine(newData);
+
+        }
+        static void sumOfPrimeNumber()
+        {
+            int n, i,  k, sum = 0;
+            bool result = false;
+            Console.Write("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 2; i <= n; i++)
+            {
+                k = 2;
+                result = true;
+                while (k < i)
+                {
+                    if (i % k == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                    k++;
+                }
+                if (result)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine("Sum of prime numbers:" + sum);
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void sumOfDegiete()
+
+        {
+            Console.WriteLine("please provided ihe input number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (n % 10 != 0)
+            {
+
+                sum = sum + n % 10;
+                n = n / 10;
+            }
+
+            Console.WriteLine("sum of the input number" + sum);
+        }
+
+
+        public static void Array()
+        {
+
+
+            int[] array1 = { 1, 3, -5, 4 };
+            int[] array2 = { 1, 4, -5, -2 };
+
+            // array1 = Convert.(Console.ReadLine());
+            Console.WriteLine(string.Join(",", "array1"));
+            Console.WriteLine(string.Join(",", "array2"));
+            Console.WriteLine("\nMultiply corresponding elements of two arrays: ");
+            for (int i = 0; i < array1.Length; i++)
+            {
+
+                int array = array1[i] * array2[i];
+
+                Console.Write(" " + array);
+            }
+
+
+
+
+        }
+    }
+}
